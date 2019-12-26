bdea0a835110fe478a62e61843cf6b1441351944
Cesar Delgado <cdelgado@altice.com.do>
2019-12-26 13:46:13 -0400

    udp

---
+++ Angular101/Angular101/Controllers/QuestionController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Angular101.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Angular101.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuestionController : ControllerBase
+    {
+        // GET api/question/all
+        [HttpGet("All/{quizId}")]
+        public IActionResult All(int quizId)
+        {
+            var sampleQuestions = new List<QuestionViewModel>();
+            // add a first sample question
+            sampleQuestions.Add(new QuestionViewModel()
+            {
+                Id = 1,
+                QuizId = quizId,
+                Text = "What do you value most in your life?",
+                CreatedDate = DateTime.Now,
+                LastModifiedDate = DateTime.Now
+            });
+
+            // add a bunch of other sample questions
+            for (int i = 2; i <= 5; i++)
+            {
+                sampleQuestions.Add(new QuestionViewModel()
+                {
+                    Id = i,
+                    QuizId = quizId,
+                    Text = String.Format("Sample Question {0}", i),
+                    CreatedDate = DateTime.Now,
+                    LastModifiedDate = DateTime.Now
+                });
+            }
+
+            // output the result in JSON format
+            return new JsonResult(sampleQuestions, new JsonSerializerOptions
+                                    {
+                                        WriteIndented = true
+                                    });
+        }
+    }
+}
---
+++ Angular101/Angular101/Controllers/QuizController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Angular101.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Angular101.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuizController : ControllerBase
+    {
+        [HttpGet("Latest/{num}")]
+        public IActionResult Latest(int num = 10)
+        {
+            var sampleQuizzes = new List<QuizViewModel>();
+
+            sampleQuizzes.Add(new QuizViewModel()
+            {
+                Id = 1,
+                Title = "Which Shingeki No Kyojin character are you?",
+                Description = "Anime-related personality test",
+                CreatedDate = DateTime.Now,
+                LastModifiedDate = DateTime.Now
+            });
+
+
+            for (int i = 2; i <= num; i++)
+            {
+                sampleQuizzes.Add(new QuizViewModel()
+                {
+                    Id = i,
+                    Title = String.Format("Sample Quiz {0}", i),
+                    Description = "This is a sample quiz",
+                    CreatedDate = DateTime.Now,
+                    LastModifiedDate = DateTime.Now
+                });
+            }
+
+            return new JsonResult(sampleQuizzes, serializerSettings: new JsonSerializerOptions() { WriteIndented = true });
+        }
+
+        /// <summary>
+        /// GET: api/quiz/ByTitle
+        /// Retrieves the {num} Quizzes sorted by Title (A to Z)
+        /// </summary>
+        /// <param name="num">the number of quizzes to retrieve</param>
+        /// <returns>{num} Quizzes sorted by Title</returns>
+        [HttpGet("ByTitle/{num:int?}")]
+        public IActionResult ByTitle(int num = 10)
+        {
+            var sampleQuizzes = ((JsonResult)Latest(num)).Value  as List<QuizViewModel>;
+
+            return new JsonResult(sampleQuizzes.OrderBy(t => t.Title), new JsonSerializerOptions() { WriteIndented = true });
+
+        }
+
+        /// <summary>
+        /// GET: api/quiz/mostViewed
+        /// Retrieves the {num} random Quizzes
+        /// </summary>
+        /// <param name="num">the number of quizzes to retrieve</param>
+        /// <returns>{num} random Quizzes</returns>
+        [HttpGet("Random/{num:int?}")]
+        public IActionResult Random(int num = 10)
+        {
+            var sampleQuizzes = ((JsonResult)Latest(num)).Value as List<QuizViewModel>;
+
+            return new JsonResult(sampleQuizzes.OrderBy(t => Guid.NewGuid()), new JsonSerializerOptions() { WriteIndented = true });
+
+        }
+
+    }
+}
